8f71680498f015f0f59897b7467c4eb4208b753d
Pete Graff <pgraff@uw.edu>
2018-10-10 14:19:02 -0700

    updated formatting to Google JS styleguide consistency, fixed deprecated event property (which)

--- src/App.js
+++ src/App.js
@@ -8,137 +8,112 @@ import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons
 library.add(faChevronRight, faChevronLeft)
 
 class App extends Component {
-    constructor() {
-      super();
-      this.state = {
-        currentSlide: 'Slide1',
-        currentTitle: 'Setup',
-        currentShortTitle: 'Setup',
-        currentNumber: null,
-        currentLevel: null,
-        currentDetailLink: null,
-        index: 0,
-        slideFocus: false
-      };
-      this.updateState = this.updateState.bind(this);
-      this.focusedSlide = React.createRef(); //set up to focus slide when nav button clicked
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSlide: 'Slide1',
+      currentTitle: 'Setup',
+      currentShortTitle: 'Setup',
+      currentNumber: null,
+      currentLevel: null,
+      currentDetailLink: null,
+      index: 0,
+      slideFocus: false
+    };
+    this.updateState = this.updateState.bind(this);
+    this.focusedSlide = React.createRef(); //set up to focus slide when nav button clicked
+  }
+
+  updateState(currentSlide, currentTitle, currentShortTitle, currentNumber, currentLevel, currentDetailLink, index, slideFocus) {
+    this.setState ({
+      currentSlide: currentSlide,
+      currentTitle: currentTitle,
+      currentShortTitle: currentShortTitle,
+      currentNumber: currentNumber,
+      currentLevel: currentLevel,
+      currentDetailLink: currentDetailLink,
+      index: index,
+      slideFocus: slideFocus
+    });
+    if(slideFocus === true) {//note errored on ternary expression in this context so doing traditional
+      this.focusedSlide.current.focus();
+    } else {
+      return null;
     }
-    updateState( currentSlide, currentTitle, currentShortTitle, currentNumber, currentLevel, currentDetailLink, index, slideFocus) {
+  }
 
-      this.setState ({
-          currentSlide: currentSlide,
-          currentTitle: currentTitle,
-          currentShortTitle: currentShortTitle,
-          currentNumber: currentNumber,
-          currentLevel: currentLevel,
-          currentDetailLink: currentDetailLink,
-          index: index,
-          slideFocus: slideFocus
-      });
-      if(slideFocus === true) {//note errored on ternary expression in this context so doing traditional
-        this.focusedSlide.current.focus();
-      } else {
-        return null;
-      }
-    }
+  render () {
+    const { currentSlide, currentTitle, currentNumber, currentLevel, currentDetailLink, index } = this.state;
+    const slidesInfo = [
+      {'slide': 'Slide1', 'title':'Setup', 'shortTitle':'Setup', 'number':null, 'level':null, 'detailLink':null },
+      {'slide': 'Slide2', 'title':'Deploy locally and to Github page', 'shortTitle':'Deploy', 'number':null, 'level':null, 'detailLink':null },
+    ];
+    const totalSlides = slidesInfo.length;
+  
+    let prevIndex;
+    let nextIndex;
 
-    render () {
-      const { currentSlide, currentTitle, currentNumber, currentLevel, currentDetailLink, index } = this.state;
-      const slidesInfo = [
-        {'slide': 'Slide1', 'title':'Setup', 'shortTitle':'Setup', 'number':null, 'level':null, 'detailLink':null },
-        {'slide': 'Slide2', 'title':'Deploy locally and to Github page', 'shortTitle':'Deploy', 'number':null, 'level':null, 'detailLink':null },
-      ];
-    
-      let prevIndex;
-      let nextIndex;
-      const totalSlides = slidesInfo.length;
+    index === 0 ? prevIndex = totalSlides - 1 : prevIndex = index - 1
+    index === totalSlides - 1 ? nextIndex = 0 : nextIndex = index + 1
 
-      index === 0
-      ?
-      prevIndex = totalSlides - 1
-      :
-      prevIndex = index - 1
-
-
-      index === totalSlides - 1
-      ?
-      nextIndex = 0
-      :
-      nextIndex = index + 1
-
-      var WCAGLink =  <a href={"https://www.w3.org/WAI/WCAG21/Understanding/" + currentDetailLink }>{ currentTitle } {currentNumber} ({currentLevel}) details and exceptions</a>;
-      var shortWCAGLink = "https://www.w3.org/WAI/WCAG21/Understanding/" + currentDetailLink;
-      var SlideToRender = Slides[currentSlide] = require('./slides/' + currentSlide).default;
-      var navList = slidesInfo.map( (slideInfo, index) => {
+    var WCAGLink = <a href={"https://www.w3.org/WAI/WCAG21/Understanding/" + currentDetailLink}>{currentTitle} {currentNumber} ({currentLevel}) details and exceptions</a>;
+    var shortWCAGLink = "https://www.w3.org/WAI/WCAG21/Understanding/" + currentDetailLink;
+    var SlideToRender = Slides[currentSlide] = require('./slides/' + currentSlide).default;
+    var navList = slidesInfo.map( (slideInfo, index) => {
       return (
-        <li key={ slideInfo.slide } className="w-TipContainer">
+        <li key={slideInfo.slide} className="w-TipContainer">
           <Button 
-            slide={ slideInfo.slide }
-            title= { slideInfo.title }
-            shortTitle = { slideInfo.shortTitle }
-            number= { slideInfo.number }
-            level= { slideInfo.level }
-            detailLink= { slideInfo.detailLink }
-            index = { index }
-            buttonText= { index + 1 } 
+            slide={slideInfo.slide}
+            title={slideInfo.title}
+            shortTitle={slideInfo.shortTitle}
+            number={slideInfo.number}
+            level={slideInfo.level}
+            detailLink={slideInfo.detailLink}
+            index={index}
+            buttonText={index + 1} 
             updateState={this.updateState} 
-            active = {
-              slideInfo.slide === currentSlide
-              ?
-              true
-              :
-              false
-            }
-            />
+            active={slideInfo.slide === currentSlide ? true : false}
+          />
         </li>
       );
     });
-      return (
-        <div>
 
+    return (
+      <div>
         <main>
           <div id="wid-HeaderNavBlock">
             <h1>HTML slideshow created with React JS</h1> 
-            
             <nav className="w-SlideNav" aria-label="Slide menu">
               <ul className="w-BtnSet">         
                 { navList }
               </ul>
             </nav>
           </div>
-        <PrevNext 
-          prevSlide = { slidesInfo[prevIndex] }
-          nextSlide = { slidesInfo[nextIndex] }
-          prevIndex = { prevIndex }
-          nextIndex= { nextIndex }
-          updateState={ this.updateState }
-        />
+          <PrevNext 
+            prevSlide={slidesInfo[prevIndex]}
+            nextSlide={slidesInfo[nextIndex]}
+            prevIndex={prevIndex}
+            nextIndex={nextIndex}
+            updateState={ this.updateState }
+          />
           <ul className="w-SlidesContainer">
             <li 
-               key={ currentSlide }
-               tabIndex='-1'
-               ref={ this.focusedSlide }
-               >
+              key={ currentSlide }
+              tabIndex='-1'
+              ref={ this.focusedSlide }
+              >
               <h2 id={"wid-SlideTitle_" +  currentSlide }>{ currentTitle }</h2>
-                <SlideToRender  shortWCAGLink = { shortWCAGLink } />
-                
+              <SlideToRender shortWCAGLink={ shortWCAGLink } />
               <p>
-                  { 
-                  currentNumber != null
-                  ?
-                  WCAGLink
-                  :
-                  null 
-                  }
+                { currentNumber != null ? WCAGLink : null }
               </p>
-                <span aria-live="polite" aria-atomic="true">Slide { index + 1 } of { totalSlides }</span>
+              <span aria-live="polite" aria-atomic="true">Slide { index + 1 } of { totalSlides }</span>
             </li>
           </ul>
-
         </main>
       </div>
-      )
-    }
+    )
+  }
 }
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,92 +2,79 @@ import React, { Component } from 'react';
 //import ToolTip from './ToolTip';
 
 class Button extends Component {
-    constructor() {
-        super();
-        this.state = {
-          hidden: true
-        }; 
-    }
-    buttonClicked() {
-        const { slide, title, shortTitle, number, level, detailLink, index, updateState } = this.props;
-        updateState(slide, title, shortTitle, number, level, detailLink, index, true);
+  constructor(props) {
+    super(props);
+    this.state = {
+      hidden: true
+    }; 
+  }
 
-    }
-    
-    showTip() {
-        this.setState ({
-            hidden:false
-        });
-    }
+  buttonClicked() {
+    const { slide, title, shortTitle, number, level, detailLink, index, updateState } = this.props;
+    updateState(slide, title, shortTitle, number, level, detailLink, index, true);
+  }
+  
+  showTip() {
+    this.setState ({
+        hidden:false
+    });
+  }
 
-    hideTip() {
-        this.setState ({
-            hidden:true
-        }); 
+  hideTip() {
+    this.setState ({
+        hidden:true
+    }); 
+  }
+
+  checkEscape(e) {//escape out of tool tip
+    if (e.key === 'Escape') {
+      e.preventDefault();
+      this.setState ({
+        hidden: true
+      });
+      return false;
     }
-    escape(e) {//escape out of tool tip
-        if (e.which === 27) {
-            this.setState ({
-                hidden:true
-            });
-            e.preventDefault();
-            return false;
-        }
-    }
+  }
 
   render() {
     const { slide, shortTitle, buttonText, index, active } = this.props;
     const { hidden } = this.state;
     const tipID = "wid_ToolTip-" + slide;
-    const tipStyle = {
-        display: hidden ? 'none' : 'block'
-      }
+    const tipStyle = { display: hidden ? 'none' : 'block' }
 
     return (
-        <div
-            onMouseOver={this.showTip.bind(this)}
-            onMouseOut={this.hideTip.bind(this)}
-            onFocus={this.showTip.bind(this)}
-            onBlur={this.hideTip.bind(this)}
-            onKeyDown={this.escape.bind(this)}
+      <div
+        onMouseOver={this.showTip.bind(this)}
+        onMouseOut={this.hideTip.bind(this)}
+        onFocus={this.showTip.bind(this)}
+        onBlur={this.hideTip.bind(this)}
+        onKeyDown={this.checkEscape.bind(this)}
+      >
+        <button 
+          type="button" 
+          id={"wid_" + slide} 
+          className={ 
+            active === true ? "w-BtnBase w-ToolTipped mod-Active" : "w-BtnBase w-ToolTipped"
+          }
+          aria-describedby={tipID}
+          onClick={this.buttonClicked.bind(this)}
         >
-            <button 
-                type="button" 
-                id={ "wid_" + slide } 
-                className={ 
-                    active === true
-                    ?
-                    "w-BtnBase w-ToolTipped mod-Active"
-                    :
-                    "w-BtnBase w-ToolTipped"
-                }
-                aria-describedby={tipID}
-                onClick={ this.buttonClicked.bind(this) }
-            >
-                { buttonText }
-            </button>
-            <div 
-                id={tipID}
-                role="tooltip"
-                aria-hidden={ hidden }
-                className="w-ToolTip"
-                style={ tipStyle }
-                >
-                <span className="w-VisuallyHide">{ index } </span>
-                { shortTitle }
-                { 
-                    active === true
-                    ?
-                    <span className="w-VisuallyHide">Current slide</span>
-                    :
-                    null
-                }
-            </div>
+          {buttonText}
+        </button>
+        <div 
+          id={tipID}
+          role="tooltip"
+          aria-hidden={hidden}
+          className="w-ToolTip"
+          style={tipStyle}
+        >
+          <span className="w-VisuallyHide">{index}</span>
+          {shortTitle}
+          {active === true ? <span className="w-VisuallyHide">Current slide</span> : null}
         </div>
+      </div>
     );
   }
 }
-
-
 
 export default Button;
