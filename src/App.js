f1384dbc61e71324ded50c92f5b75654faed8e1a
Pete Graff <pgraff@uw.edu>
2018-10-15 11:07:57 -0700

    removing unneeded module from react-event-listener

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import EventListener, {withOptions} from 'react-event-listener';
+import EventListener from 'react-event-listener';
 import slidesInfo from './slides/slidesInfo';
 import Header from './components/Header';
 import Footer from './components/Footer';
