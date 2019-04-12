e953fdc4b87411d9dd2eeb0262c57b7e88cdb4f9
Jeane Marty <jeanem@uw.edu>
2019-04-11 19:32:58 -0700

    deleted unneeded files, code

--- src/slides/svg1Content.js
+++ src/slides/svg1Content.js
@@ -1,7 +1,5 @@
-// import React from 'react';
 import React, { Component } from 'react';
 import Prism from 'prismjs';
-//import Prism from 'prismjs';
 import '../css/prism.css';
 
 class Slide extends Component {
