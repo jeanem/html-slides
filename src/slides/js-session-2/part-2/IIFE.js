4cb32a9c1a5a702f5285ea0e3bbd68684bd61a44
Teri Shelton <tjshelt@uw.edu>
2020-11-12 17:49:57 -0800

    expanded on IIFE, updated styling

--- src/slides/js-session-2/part-2/IIFE.js
+++ src/slides/js-session-2/part-2/IIFE.js
@@ -3,42 +3,66 @@ import React from 'react';
 function Slide(props) {
   return (
     <div>
-      <p>Something about IIFE...</p>
       <p>
-        An IIFE (Immediately Invoked Function Expression - pronounced
-        <em>iffy</em>) is a JavaScript function that runs as soon as it is
-        defined.
-      </p>
-      {/* <p>
-        Arrow functions use the <code>{`=>`}</code> (arrow) syntax instead of
-        the <code>{`function`}</code> keyword.
+        An Immediately Invoked Function Expression (or IIFE - pronounced "iffy")
+        is a JavaScript function that runs as soon as it is defined.
       </p>
 
-      <p>
-        The arrow comes after the list of <em>parameters</em> and is followed by
-        the function’s <em>body</em>.
-      </p>
+      <p>It is also known as a Self-Executing Anonymous Function.</p>
 
-      <p>
-        Can be read as: “This input (parameters) produces this result (body).”
-      </p>
-
-      <h3>Example</h3>
       <pre>
         <code>
           {`
-const power = (base, exponent) => {
-  let result = 1;
-  for (let count = 0; count < exponent; count++) {
-    result *= base;
-  }
-  return result;
-};
-
-power(10, 5);
+(function () {
+  statements
+})();
         `}
         </code>
-      </pre> */}
+      </pre>
+      <ol>
+        <li>
+          The first part is an anonymous function and is enclosed within the
+          <code>{`Grouping Operator ()`}</code>. This prevents external access
+          to the variables inside the IIFE, as well as polluting the global
+          scope.
+        </li>
+        <li>
+          The second part creates the immediately invoked function expression
+          <code>{`()`}</code> through which the JavaScript engine will directly
+          interpret the function.
+        </li>
+      </ol>
+
+      <h3>Examples</h3>
+      <pre>
+        <code>
+          {`
+(function () {
+  var aName = "Barry";
+})();
+// Variable aName is not accessible from the outside scope
+aName // throws "Uncaught ReferenceError: aName is not defined"
+        `}
+        </code>
+      </pre>
+
+      <p>
+        We can also assign the IIFE to a variable. The variable only stores the
+        function's return value and not the actual function definition.
+      </p>
+
+      <pre>
+        <code>
+          {`
+var result = (function () {
+  var name = "Barry";
+  return name;
+})();
+// Immediately creates the output:
+result; // "Barry"
+        `}
+        </code>
+      </pre>
       <p>
         Source:
         <a href="https://developer.mozilla.org/en-US/docs/Glossary/IIFE">MDN</a>
