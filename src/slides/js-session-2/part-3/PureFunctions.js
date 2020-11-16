0f02d64f026bd3ce2600ac71c7c5bb82d0edc8a7
jeanem <jeanem@users.noreply.github.com>
2020-11-16 07:50:48 -0800

    pure function slide tweak

--- src/slides/js-session-2/part-3/PureFunctions.js
+++ src/slides/js-session-2/part-3/PureFunctions.js
@@ -9,12 +9,13 @@ function Slide(props) {
         <li>has no side-effects</li>
         <li>and doesn’t rely on side effects of other code</li>
         <li>always produces the same code when called with same arguments</li>
+        <li>the foundation of functional programming</li>
       </ul>
 
       <pre>
         <code>
           {`function emojiMixer(emoji1, emoji2){
-  let together = emoji1 + ' and ' + emoji2
+  let together = 'This string concatenates the first emoji: ' + emoji1 + ' and the second emoji: ' + emoji2
   return together;
 }
 emojiMixer('🐝','🐞');
